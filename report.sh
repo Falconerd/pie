a1d159e6b1d9b1f8f5d4d535963282e4d0aaaa50
Dylan Falconer <me@falconerd.com>
2023-03-23 13:20:57 +1100

    Add PNG -> SII conversion for testing

---
+++ report.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if a directory was provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 DIRECTORY"
+  exit 1
+fi
+
+# Find all .sii files in the directory
+FILES=$(ls "$1"/*.sii 2>/dev/null)
+
+# Loop through each .sii file and compare its size to other .sii files with the same stem
+for FILE in $FILES; do
+  # Get the file stem by removing the extension
+  STEM=$(basename "$FILE" | cut -d'.' -f1)
+
+  # Check if there are other .sii files with the same stem
+  OTHER_FILES=$(ls "$1/$STEM"*.png 2>/dev/null)
+  if [ -n "$OTHER_FILES" ]; then
+    # Loop through each other .sii file and compare its size to the current .sii file
+    for OTHER_FILE in $OTHER_FILES; do
+      if [ "$FILE" != "$OTHER_FILE" ]; then
+        SIZE1=$(wc -c <"$FILE")
+        SIZE2=$(wc -c <"$OTHER_FILE")
+        DIFF=$(expr $SIZE1 - $SIZE2)
+        PERCENT=$(echo "scale=2; $DIFF / $SIZE1 * 100" | bc)
+        echo "$FILE: $SIZE1 bytes; $OTHER_FILE: $SIZE2 bytes; Difference: $DIFF bytes ($PERCENT%)"
+      fi
+    done
+  fi
+done
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use std::collections::HashMap;
+use std::env;
+use std::env::Args;
+use std::fs::File;
 use std::fs::read_to_string;
+use std::io::Write;
+use std::path::PathBuf;
+use std::process::exit;
 
 #[derive(Debug)]
 struct DecodedSII {
@@ -60,7 +67,7 @@ fn encode_rle(data: &[u8]) -> Vec<u8> {
     let mut count = 1;
 
     for i in 1..=data.len() {
-        if i < data.len() && data[i] == data[i - 1] {
+        if i < data.len() && data[i] == data[i - 1] && count < 255 {
             count += 1;
         } else {
             encoded.push(count);
@@ -113,6 +120,147 @@ fn encode(version: u8, width: u16, height: u16, compress: bool, has_alpha: bool,
     }
 
     return Ok(bytes);
+}
+
+// For now assume RGBA.
+fn palette_from_pixels(bytes: &[u8]) -> Vec<u32> {
+    let mut exists = HashMap::<u32, ()>::new();
+    let mut palette = Vec::<u32>::new();
+    for (index, _) in bytes.iter().enumerate().step_by(4) {
+        let r = bytes[index] as u32;
+        let g = bytes[index + 1] as u32;
+        let b = bytes[index + 2] as u32;
+        let a = bytes[index + 3] as u32;
+        let rgba = r << 24 | g << 16 | b << 8 | a;
+        if exists.get(&rgba).is_none() {
+            exists.insert(rgba, ());
+            palette.push(rgba);
+        }
+    }
+    return palette;
+}
+
+#[derive(PartialEq)]
+enum ColorFormat {
+    RGBA,
+    BGRA,
+    RGB,
+    BGR,
+}
+
+fn encode_from_bytes(version: u8, width: u16, height: u16, bytes: &[u8], format: ColorFormat, compress: bool, embed_palette: bool) -> Result<Vec<u8>, EncodeError> {
+    let mut exists: HashMap<u32, u8> = HashMap::new();
+    let mut palette: Vec<u32> = vec![];
+
+    let has_alpha = format == ColorFormat::RGBA || format == ColorFormat::BGRA;
+
+    let w = width.to_be_bytes();
+    let h = height.to_be_bytes();
+    let mut flags: u8 = 0;
+    if compress {
+        flags |= 0b1;
+    }
+    if embed_palette {
+        flags |= 0b10;
+    }
+    if has_alpha {
+        flags |= 0b100;
+    }
+    let mut out: Vec<u8> = vec![0x53, 0x49, 0x49, version, w[0], w[1], h[0], h[1], flags];
+    let mut out_bytes: Vec<u8> = vec![];
+
+    let mut palette_index: u8 = 0;
+
+    for (index, _) in bytes.iter().enumerate().step_by(4) {
+        // Pixels always stored in RGBA anyway because palettes are.
+        let pixel = match &format {
+            ColorFormat::RGBA => (bytes[index + 0] as u32) << 24 |
+                                 (bytes[index + 1] as u32) << 16 |
+                                 (bytes[index + 2] as u32) <<  8 |
+                                 (bytes[index + 3] as u32),
+            ColorFormat::RGB =>  (bytes[index + 0] as u32) << 24 |
+                                 (bytes[index + 1] as u32) << 16 |
+                                 (bytes[index + 2] as u32) <<  8 | 0xFF,
+            ColorFormat::BGRA => (bytes[index + 0] as u32) <<  8 |
+                                 (bytes[index + 1] as u32) << 16 |
+                                 (bytes[index + 2] as u32) << 24 |
+                                 (bytes[index + 3] as u32),
+            ColorFormat::BGR =>  (bytes[index + 0] as u32) <<  8 |
+                                 (bytes[index + 1] as u32) << 16 |
+                                 (bytes[index + 2] as u32) << 24 | 0xFF,
+        };
+
+        if exists.get(&pixel).is_none() {
+            exists.insert(pixel, palette_index);
+            palette.push(pixel);
+            if palette_index == 255 {
+                println!("Too many colours. Max is 256. Exiting.");
+                exit(1);
+            }
+            palette_index += 1;
+        }
+
+        if let Some(i) = exists.get(&pixel) {
+            out_bytes.push(*i);
+        }
+    }
+
+    if compress {
+        let mut compressed = encode_rle(&out_bytes);
+        out.append(&mut compressed);
+    } else {
+        out.append(&mut out_bytes);
+    }
+
+    if embed_palette {
+        out.push(0x00);
+
+        for color in palette {
+            let color_bytes = color.to_le_bytes();
+            let mut hex: Vec<u8> = vec![color_bytes[2], color_bytes[1], color_bytes[0]];
+            if has_alpha {
+                hex.insert(0, color_bytes[3]);
+            }
+            out.append(&mut hex);
+        }
+    }
+
+    Ok(out)
+}
+
+// Convert args[1] from PNG into SII with palette included
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    match args[1].as_str() {
+        "-e" | "--encode" => do_encode(&args),
+        "-d" | "--decode" => do_decode(&args),
+        _ => usage(&args),
+    }
+}
+
+fn do_encode(args: &Vec<String>) {
+    let filename = PathBuf::from(&args[2]);
+    let file_stem = filename.file_stem().unwrap().to_str().unwrap();
+    let decoder = png::Decoder::new(File::open(&filename).unwrap());
+    let mut reader = decoder.read_info().unwrap();
+    let mut buf: Vec<u8> = vec![0; reader.output_buffer_size()];
+    let info = reader.next_frame(&mut buf).unwrap();
+    let bytes = &buf[..info.buffer_size()];
+    let encoded = encode_from_bytes(1, reader.info().width as u16, reader.info().height as u16, bytes, ColorFormat::RGBA, true, true);
+    let mut file = File::create(filename.with_extension("sii")).expect("Could not create file");
+    file.write_all(&encoded.unwrap());
+}
+
+fn do_decode(args: &Vec<String>) {
+}
+
+fn usage(args: &Vec<String>) {
+    println!("USAGE:\n\t{} [OPTIONS] FILE", &args[0]);
+    println!("\nOPTIONS:");
+    println!("\t-e, --encode\tEncode the input file");
+    println!("\t-d, --decode\tDecode the input file");
+    println!("\nARGS:");
+    println!("\tFILE\tInput file to be encoded or decoded:");
 }
 
 fn test(path: &str, palette: &Vec<u32>) -> DecodedSII {
